3ba520df38f8976077c2b6965f0dc8165412317d
David Ahern <dsahern@gmail.com>
2020-02-14 10:08:25 -0700

    set-governor: Add status option
    Add status option to show current state of cpu governor.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- scripts/set-governor.sh
+++ scripts/set-governor.sh
@@ -4,9 +4,20 @@
 #
 # David Ahern <dsahern@gmail.com>
 
+show_gov()
+{
+	local f
+
+	find /sys/devices/system/cpu/cpufreq -name scaling_governor |
+	while read f; do
+		cat $f
+	done
+}
+
 case $1 in
 	perf*) gov=performance;;
 	power*) gov=powersave;;
+	show|stat|status) show_gov; exit 0;;
 	*) echo "unknown governor"; exit 1;;
 esac
 
