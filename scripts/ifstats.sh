4ae0321dd1f9e275a4e82d33954be8a000916886
David Ahern <dsahern@gmail.com>
2020-02-14 10:06:40 -0700

    ifstats: Add header and let sample time be configurable
    Add a header for column meaning, and repeat every 64 rows (terminal
    height to have header show at least once).
    
    Allow sample time to be configurable.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- scripts/ifstats.sh
+++ scripts/ifstats.sh
@@ -6,9 +6,25 @@
 
 export LC_NUMERIC=""
 
+show_header()
+{
+	printf "Stats for ${DEV} over last ${DT} seconds\n"
+	printf "\n"
+	printf "%8s "   "Time"
+	printf "Rx: %11s   %7s   %6s   %5s"  "Bps" "pps" "avg-sz" "drops"
+	printf "    "
+	printf "Tx: %11s   %7s   %6s   %5s"  "Bps" "pps" "avg-sz" "drops"
+	printf "\n"
+}
+
+
 DEV=${1}
 [ -z $DEV ] && DEV=eth0
-declare -i ITER=${2}
+
+DT=${2}
+[ -z "${DT}" -o "${DT}"  = "0" ] && DT=1
+
+declare -i ITER=${3}
 
 S=$(awk -v dev="${DEV}:" '$1 == dev {print}' /proc/net/dev)
 
@@ -21,9 +37,9 @@ TXB_P=${10}
 TXP_P=${11}
 TD_P=${13}
 
-sleep 1
+sleep ${DT}
 
-printf "Stats for ${DEV}\n"
+declare -i k=1
 while [ 1 ]; do
 	T=$(date +%T)
 	S=$(awk -v dev="${DEV}:" '$1 == dev {print}' /proc/net/dev)
@@ -55,16 +71,23 @@ while [ 1 ]; do
 		tpktsz=$(($tbytes / $tpkts))
 	fi
 
-	printf "$T "
-	printf "Rx: %'11d   %'7d   %'5d   %'5d"   ${rbytes} ${rpkts} ${rpktsz} ${rdrop}
+	k=$((k-1))
+	if [ $k -eq 0 ]; then
+		show_header
+		k=64
+	fi
+	printf "%8s " $T
+	printf "Rx: %'11d   %'7d   %'6d   %'5d"   $((${rbytes}/${DT})) $((${rpkts}/${DT})) ${rpktsz} ${rdrop}
 	printf "    "
-	printf "Tx: %'11d   %'7d   %'5d   %'5d\n" ${tbytes} ${tpkts} ${tpktsz} ${tdrop}
+	printf "Tx: %'11d   %'7d   %'6d   %'5d"   $((${tbytes}/${DT})) $((${tpkts}/${DT})) ${tpktsz} ${tdrop}
+	printf "\n"
 
 	if [ ${ITER} -gt 0 ]; then
 		ITER=$((ITER-1))
 		[ ${ITER} -eq 0 ] && break
 	fi
-	sleep 1
+
+	sleep ${DT}
 
 	RD_P=$RD
 	RXB_P=$RXB
