36827b9bef82f60eecddef394f5838d3ba32d691
David Ahern <dsahern@gmail.com>
2020-10-30 16:24:42 -0600

    pktgen: speed up checksum computation
    Borrow asm based checksum functions from kernel to speed up
    checksum computations.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- src/pktgen.c
+++ src/pktgen.c
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: GPL-2.0
 /* Simple packet generator
  *
  * Ethernet:
@@ -610,60 +611,163 @@ static int ipv4_parse(int argc, char *argv[])
 	}
 	return 0;
 }
-
-static unsigned short ipv4_csum_fold(unsigned long csum)
+/*
+ * following checksum code is from kernel,
+ * - arch/x86/lib/csum-partial_64.c and
+ * - arch/x86/include/asm/checksum_64.h
+ */
+static __u16 csum_fold(__u32 csum)
 {
-	csum = (csum >> 16) + (csum & 0xFFFF);
-	csum = ~((csum >> 16) + csum) & 0xFFFF;
-
-	return csum;
+        asm("  addl %1,%0\n"
+            "  adcl $0xffff,%0"
+            : "=r" (csum)
+            : "r" (csum << 16),
+              "0" (csum & 0xffff0000));
+        return (__u16)(~csum >> 16);
 }
 
-static unsigned long __ipv4_csum(const void *buf, int nbytes,
-				 unsigned long csum)
+static inline unsigned add32_with_carry(unsigned a, unsigned b)
 {
-	const unsigned short *p = buf;
-	int n = nbytes >> 1;
+	asm("addl %2,%0\n\t"
+	    "adcl $0,%0"
+	    : "=r" (a)
+	    : "0" (a), "rm" (b));
+	return a;
+}
 
-	for (; n > 0; n--)
-		csum += *p++;
+static inline unsigned short from32to16(unsigned a)
+{
+	unsigned short b = a >> 16;
 
-	/* 0 pad odd bytes */
-	if (nbytes & 1) {
-		const __u8 *pbuf = buf;
-		__u8 buf2[2];
+	asm("addw %w2,%w0\n\t"
+	    "adcw $0,%w0\n"
+	    : "=r" (b)
+	    : "0" (b), "r" (a));
+	return b;
+}
 
-		buf2[0] = pbuf[nbytes - 1];
-		buf2[1] = 0;
-		p = (unsigned short *)buf2;
-		csum += *p;
+static unsigned do_csum(const unsigned char *buff, unsigned len)
+{
+	unsigned odd, count;
+	unsigned long result = 0;
+
+	if (len == 0)
+		return result; 
+	odd = 1 & (unsigned long) buff;
+	if (odd) {
+		result = *buff << 8;
+		len--;
+		buff++;
 	}
+	count = len >> 1;		/* nr of 16-bit words.. */
+	if (count) {
+		if (2 & (unsigned long) buff) {
+			result += *(unsigned short *)buff;
+			count--;
+			len -= 2;
+			buff += 2;
+		}
+		count >>= 1;		/* nr of 32-bit words.. */
+		if (count) {
+			unsigned long zero;
+			unsigned count64;
+			if (4 & (unsigned long) buff) {
+				result += *(unsigned int *) buff;
+				count--;
+				len -= 4;
+				buff += 4;
+			}
+			count >>= 1;	/* nr of 64-bit words.. */
 
-	return csum;
+			/* main loop using 64byte blocks */
+			zero = 0;
+			count64 = count >> 3;
+			while (count64) { 
+				asm("addq 0*8(%[src]),%[res]\n\t"
+				    "adcq 1*8(%[src]),%[res]\n\t"
+				    "adcq 2*8(%[src]),%[res]\n\t"
+				    "adcq 3*8(%[src]),%[res]\n\t"
+				    "adcq 4*8(%[src]),%[res]\n\t"
+				    "adcq 5*8(%[src]),%[res]\n\t"
+				    "adcq 6*8(%[src]),%[res]\n\t"
+				    "adcq 7*8(%[src]),%[res]\n\t"
+				    "adcq %[zero],%[res]"
+				    : [res] "=r" (result)
+				    : [src] "r" (buff), [zero] "r" (zero),
+				    "[res]" (result));
+				buff += 64;
+				count64--;
+			}
+
+			/* last up to 7 8byte blocks */
+			count %= 8; 
+			while (count) { 
+				asm("addq %1,%0\n\t"
+				    "adcq %2,%0\n" 
+					    : "=r" (result)
+				    : "m" (*(unsigned long *)buff), 
+				    "r" (zero),  "0" (result));
+				--count; 
+				buff += 8;
+			}
+			result = add32_with_carry(result>>32,
+						  result&0xffffffff); 
+
+			if (len & 4) {
+				result += *(unsigned int *) buff;
+				buff += 4;
+			}
+		}
+		if (len & 2) {
+			result += *(unsigned short *) buff;
+			buff += 2;
+		}
+	}
+	if (len & 1)
+		result += *buff;
+	result = add32_with_carry(result>>32, result & 0xffffffff); 
+	if (odd) { 
+		result = from32to16(result);
+		result = ((result >> 8) & 0xff) | ((result & 0xff) << 8);
+	}
+	return result;
 }
 
-static unsigned short ipv4_csum(const void *buf, short nbytes)
+__u32 csum_partial(const void *buff, int len, __u32 sum)
 {
-	return ipv4_csum_fold(__ipv4_csum(buf, nbytes, 0));
+        return (__u32)add32_with_carry(do_csum(buff, len), sum);
 }
 
-static unsigned short tcpudp_csum(__be32 sip, __be32 dip, unsigned char proto,
-				  const unsigned char *buf, unsigned int len)
+static __u16 ipv4_csum(const void *buf, short nbytes)
 {
-	unsigned char tcpbuf[12];   /* pseudo-header + tcp header */
-	unsigned long csum;
-	__u16 *plen;
+	return csum_fold(csum_partial(buf, nbytes, 0));
+}
 
-	memcpy(tcpbuf, &sip, 4);
-	memcpy(tcpbuf + 4, &dip, 4);
-	tcpbuf[8] = 0;
-	tcpbuf[9] = proto;
-	plen = (__u16 *)&tcpbuf[10];
-	*plen = htons(len);
+static inline __u32 csum_tcpudp_nofold(__be32 saddr, __be32 daddr,
+				       __u32 len, __u8 proto, __u32 sum)
+{
+	asm("  addl %1, %0\n"
+	    "  adcl %2, %0\n"
+	    "  adcl %3, %0\n"
+	    "  adcl $0, %0\n"
+	    : "=r" (sum)
+	    : "g" (daddr), "g" (saddr),
+	    "g" ((len + proto)<<8), "0" (sum));
+	return sum;
+}
 
-	csum = __ipv4_csum(tcpbuf, 12, 0UL);
-	csum = __ipv4_csum(buf, len, csum);
-	return ipv4_csum_fold(csum);
+/*
+ * end copy from kernel
+ */
+
+static __u16 tcpudp_csum(__be32 sip, __be32 dip, unsigned char proto,
+			 const unsigned char *buf, unsigned int len)
+{
+	__u32 csum;
+
+	csum = csum_tcpudp_nofold(sip, dip, len, proto, 0);
+	csum = csum_partial(buf, len, csum);
+	return csum_fold(csum);
 }
 
 static int fill_ipv4_hdr(void *buf, int buflen,
